74b58a9776cd412d82c45462d9d722876cd54836
Jealous Matsikachando <pasej5@gmail.com>
2023-09-18 19:32:47 -0700

    Import fix

--- models/city.py
+++ models/city.py
@@ -2,9 +2,13 @@
 """ City Module for HBNB project """
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel
+from models.base_model import Base, declarative_base
 
 
-class City(BaseModel):
+Base = declarative_base()
+
+
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
-from models.base import Base
 from models.city import City
 
 
