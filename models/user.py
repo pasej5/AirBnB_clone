b532798d17d43c529c0712afbaec44d48b82b34e
chukwucyprian <chukwucyprian262@gmail.com>
2023-09-20 01:40:09 -0700

    models correction

--- models/user.py
+++ models/user.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-    """
-    Definition of the User class for the users table in
-    the database.
-
+    """This is the class for user
     Attributes:
-        __tablename__ : this represents "users".
-        email : This represents a column (128 char), cant be null
-        password : This represnts a column (128), cant be null
-        first_name : This reprents a Column (128 char)
-        can be null
-        last_name : This represnts a column (128 char)
-        can be null
-
-    Args:
-        BaseModel : the Basemodel class
-        Base : declarative class
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
