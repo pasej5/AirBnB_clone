1dd637329254c0f1d970e5da3c6bdf62622388c3
Jealous Matsikachando <pasej5@gmail.com>
2023-09-20 00:39:31 -0700

    Console improvements

--- models/base_model.py
+++ models/base_model.py
@@ -19,13 +19,17 @@ class BaseModel:
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = self.updated_at = datetime.utcnow()
             storage.new(self)
         else:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
+            if 'created_at' not in kwargs:
+                self.updated_at = datetime.utcnow()
+            if 'updated_at ' not in kwargs:
+                self.updated_at = datetime.utcnow()
+
+        for key, value in kwargs.items():
+            if key != '__class__':
+                setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -36,6 +40,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.utcnow()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
