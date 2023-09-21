520060770258ca26a918a6b1dff5a4a5e5eb52ef
Jealous Matsikachando <pasej5@gmail.com>
2023-09-20 19:16:56 -0700

    db storage connection fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -16,17 +16,15 @@ class DBStorage:
 
     def __init__(self):
         """Initialize DBStorage instance"""
-        db_user = getenv("HBNB_MYSQL_USER")
-        db_pwd = getenv("HBNB_MYSQL_PWD")
-        db = getenv("HBNB_MYSQL_DB")
-        db_host = getenv("HBNB_MYSQL_HOST")
-        environment = getenv("HBNB_ENV")
-        connection_url = f"mysql+mysql://{db_user}:{db_pwd}@{db_host}/db"
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(getenv('HBNB_MYSQL_USER'),
+                                             getenv('HBNB_MYSQL_PWD'),
+                                             getenv('HBNB_MYSQL_HOST'),
+                                             getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
 
-        self.__engine = create_engine(connection_url, pool_pre_ping=True)
-
-        if environment == 'test':
-            metadata.drop_all(bind=self._engine)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """Query all objects depending on class name"""
