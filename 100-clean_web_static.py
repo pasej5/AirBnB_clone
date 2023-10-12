67b7ecbe4ec818b4b9e5986b76e20949d840cfdb
Jealous Matsikachando <pasej5@gmail.com>
2023-10-12 01:06:42 -0700

    do clean

---
+++ 100-clean_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Fabric script to clean out-of-date archives.
+"""
+
+from fabric.api import run, local, env
+import os
+
+env.hosts = ['<IP web-01>', '<IP web-02>']
+
+def do_clean(number=0):
+    """
+    Deletes out-of-date archives.
+    """
+    number = int(number)
+    if number < 2:
+        number = 1
+    local_archives = local("ls -t versions", capture=True).split('\n')
+    for archive in local_archives[number:]:
+        local("rm -f versions/{}".format(archive))
+
+
+    remote_archives = run("ls -t /data/web_static/releases").split()
+    for archive in remote_archives[number:]:
+        run("rm -rf /data/web_static/releases/{}".format(archive))
