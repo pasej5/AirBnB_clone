f566ad4ceb132c21f1ecef0f20d14cd5d020502b
Jealous Matsikachando <pasej5@gmail.com>
2023-10-22 12:45:09 -0700

    Python is cool!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Prints a Message when / is called """
+    """ Prints a when root is called"""
     return 'Hello HBNB!'
 
 
@@ -18,7 +18,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """ Prints a Message when /c is called """
+    """ Prints a Text when /c is called """
     return "C " + text.replace('_', ' ')
 
 if __name__ == "__main__":
