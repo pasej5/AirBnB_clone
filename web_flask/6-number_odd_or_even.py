c1b40cd255a443133aa82b877515bbc248464d6e
Jealous Matsikachando <pasej5@gmail.com>
2023-10-22 15:37:16 -0700

    odd or even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -3,8 +3,7 @@
 starts a Flask web application
 """
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -22,31 +21,31 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display “C ” plus text variable"""
+    """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def p_is_cool(text='is cool'):
-    """Python followed by text variable"""
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def is_a_number(n):
-    """displays an integer"""
-    return "{:d} is a number".format(n)"
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def templates(n):
+def numbersandtemplates(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def numbe_and_even(n):
+def numbersandevenness(n):
     """display a HTML page only if n is an integer"""
     if n % 2 == 0:
         evenness = 'even'
@@ -56,4 +55,4 @@ def numbe_and_even(n):
                            evenness=evenness)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')`
+    app.run(host='0.0.0.0', port='5000')
