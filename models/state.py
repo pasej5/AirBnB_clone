a4bfa781b0b5028e525303b5decc88ff9ae36a3a
Jealous Matsikachando <pasej5@gmail.com>
2023-10-22 17:01:42 -0700

    improve engines

--- models/state.py
+++ models/state.py
@@ -21,8 +21,15 @@ class State(BaseModel, Base):
 
     @property
     def cities(self):
-        city_list = []
-        for city in models.storage.all(city).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-                return city_list
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (lista)
